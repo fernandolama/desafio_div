0f5182b6a42f6c00db68f77830da2ed2525448ad
Fernando A <you@example.com>
2022-10-18 15:58:53 -0300

    ajustes na parte das vendas

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+from keyword import issoftkeyword
 from flask import Flask
 from flask import request, redirect, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
-produtos = {} #{produto: [valor, quantidade]}
+
 prod_carrinho = {}
 
 catalogo = pd.read_csv('catalogo.csv', index_col='produto')
 carrinho = pd.read_csv('carrinho.csv', index_col='produto')
+
+produtos = catalogo.to_dict(orient='index')
+'''produtos = {
+    chave: list(valor.values()) 
+    for chave,valor in produtos.items()
+    }'''
+
+dicionario = {}
+for chave, valor in produtos.items():
+    dicionario[chave] = list(valor.values())
+
+produtos = dicionario.copy()
+del dicionario
+print(produtos)
 
 @app.route("/")
 def index():
@@ -67,24 +82,26 @@ def delet():
 @app.route("/adic_carrinho")
 def adic_carrinho():
     argumentos = request.args.to_dict()
-    
+                                                
     produto = argumentos['produto']
     quantidade = int(argumentos['quantidade'])
-    preco = produtos.get(produto[0])
-    
+    preco = produtos.get(produto)[0]
+    print(preco)
     #dicionário
-    prod_carrinho[produto] = [preco, quantidade]
+    prod_carrinho[produto] = [preco, quantidade] 
 
     #dataframe
     carrinho.loc[produto] = [preco, quantidade]
-
-    #Ajuste nas quantidades do dict produtos
-    produtos[produto] = [preco,int('quantidade')-int(quantidade)]
+    print(produtos)
+    #Ajuste nas quantidades do dict produtos.
+    produtos[produto] = [preco,produtos[produto][1]-quantidade]
+    #Ajuste nas quantidades do df catalogo.
+    catalogo.loc[produto,'quantidade'] = catalogo.loc[produto,'quantidade']-quantidade
     return redirect('/carr')
 
 @app.route('/carr')
 def carr():
-     return render_template('carrinho.html', carrinho=carrinho.to_html())
+    return render_template('carrinho.html', carrinho=carrinho.to_html())
 
 @app.route('/remov_carrinho')
 def remov_carrinho():
@@ -92,17 +109,28 @@ def remov_carrinho():
     
     produto = argumentos['produto']
     quantidade = argumentos['quantidade']
-    preco = produtos.get(produto[0])
+    preco = produtos.get(produto)[0]
 
-    #dicionário
-    prod_carrinho[produto] = [preco, int('quantidade')-int((quantidade))]
+    #dicionário    
+    prod_carrinho[produto] = [preco, quantidade] 
 
-    #Ajuste nas quantidades do dict produtos
-    produtos[produto] = [preco,int('quantidade')+int(quantidade)]
+    #dataframe
+    carrinho.loc[produto] = [preco, quantidade]
+    print(produtos)
+    #Ajuste nas quantidades do dict produtos.
+    produtos[produto] = [preco,produtos[produto][1]+quantidade]
+    #Ajuste nas quantidades do df catalogo.
+    catalogo.loc[produto,'quantidade'] = catalogo.loc[produto,'quantidade']+quantidade
+
     return redirect('/carr')
 
-@app.route('/final_venda')
-def final_venda():
+@app.route('/final_vend')
+def final_vend():
+    
+    
+    
+    catalogo.to_csv('catalogo.csv')
+    
     return redirect('static/vendido.html')
 
 if __name__ == "__main__":
